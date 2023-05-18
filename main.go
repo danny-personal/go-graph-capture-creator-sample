04897531c5e9e1da2fe6807cc9adc1ae582da2e7
danny-yamamoto <yamamoto_daisuke@retail-ai.jp>
2023-05-18 06:16:43 +0000

    Add Aggregation

--- main.go
+++ main.go
@@ -17,20 +17,30 @@ import (
 func Chart(w http.ResponseWriter, r *http.Request) {
 	ctx := context.Background()
 	keyJson := os.Getenv("KEY_JSON")
-
+	if keyJson == "" {
+		fmt.Println("There's no KEY_JSON.")
+		return
+	} else {
+		fmt.Println("KEY_JSON: OK")
+	}
 	svc, err := monitoring.NewService(ctx, option.WithCredentialsJSON([]byte(keyJson)))
 	if err != nil {
 		fmt.Printf("Failed to create service. %v", err)
 	}
-
 	projectID := "junior-engineers-gym-2023"
 	filter := `metric.type="compute.googleapis.com/instance/cpu/utilization"`
 
 	location, _ := time.LoadLocation("Asia/Tokyo")
 	now := time.Now().In(location)
-	startTime := now.Add(-3 * time.Hour).Format(time.RFC3339)
+	startTime := now.Add(-1 * time.Hour).Format(time.RFC3339)
 	endTime := now.Format(time.RFC3339)
-	res, err := svc.Projects.TimeSeries.List("projects/" + projectID).Filter(filter).IntervalStartTime(startTime).IntervalEndTime(endTime).Do()
+	res, err := svc.Projects.TimeSeries.List("projects/" + projectID).Filter(filter).
+		IntervalStartTime(startTime).
+		IntervalEndTime(endTime).
+		AggregationPerSeriesAligner("ALIGN_MAX").
+		AggregationAlignmentPeriod("60s").
+		AggregationCrossSeriesReducer("REDUCE_MAX").
+		Do()
 	if err != nil {
 		fmt.Printf("Could not execute request: %v", err)
 	}
@@ -40,7 +50,7 @@ func Chart(w http.ResponseWriter, r *http.Request) {
 	for _, ts := range res.TimeSeries {
 		for _, point := range ts.Points {
 			if point.Value.DoubleValue != nil {
-				value := *point.Value.DoubleValue
+				value := *point.Value.DoubleValue * 100
 				timeStamp := point.Interval.EndTime
 				t, _ := time.Parse(time.RFC3339, timeStamp)
 				timeList = append(timeList, t.In(location).Format(time.RFC3339))
